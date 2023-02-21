ee734bdd7e40df5708c053d0d909bb27ab521654
梁岳喆 <85303423+LiangYuezhe001@users.noreply.github.com>
2023-02-22 01:01:38 +0800

    优化

--- compair.m
+++ compair.m
@@ -1,7 +1,7 @@
 %[test_output,test_input]=temperture_field_caculation(1000);
 nnet_output=myNeuralNetworkFunction2(test_input);
-nno=nnet_output(:,5);
+nno=nnet_output(:,4);
 plot(nno);
 hold on;
-tto=test_output(:,5);
+tto=test_output(:,4);
 plot(tto);
