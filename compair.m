0672893d7e7d0c5d81a9fe14232d33034ea237af
梁岳喆 <85303423+LiangYuezhe001@users.noreply.github.com>
2023-02-21 22:26:14 +0800

    usecomsoldata

--- compair.m
+++ compair.m
@@ -1,7 +1,7 @@
-[test_output,test_input]=temperture_field_caculation(1000);
-nnet_output=myNeuralNetworkFunction(test_input);
-nno=nnet_output(1,:);
+%[test_output,test_input]=temperture_field_caculation(1000);
+nnet_output=myNeuralNetworkFunction2(test_input);
+nno=nnet_output(:,5);
 plot(nno);
 hold on;
-tto=test_output(1,:);
+tto=test_output(:,5);
 plot(tto);
--- nnet_data_generator.m
+++ nnet_data_generator.m
@@ -1,3 +1,4 @@
+
 train_input = zeros(5,300) ;
 train_output = zeros(3,300);
 % test_input = zeros(5,100) ;
