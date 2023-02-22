f3c9557a6c0e0ed09a4414952e61ad3335630c98
梁岳喆 <85303423+LiangYuezhe001@users.noreply.github.com>
2023-02-22 19:14:43 +0800

    add nnet3

--- compair.m
+++ compair.m
@@ -1,7 +1,7 @@
 %[test_output,test_input]=temperture_field_caculation(1000);
-nnet_output=myNeuralNetworkFunction2(test_input);
-nno=nnet_output(:,4);
+nnet_output=myNeuralNetworkFunction3(test_input);
+nno=nnet_output(:,3);
 plot(nno);
 hold on;
-tto=test_output(:,4);
+tto=test_output(:,3);
 plot(tto);
